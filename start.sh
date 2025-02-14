c03886fb36f094ff1c5f8f896326ac2b07288d18
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-02-14 14:48:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Khởi động JupyterLab với quyền root
-jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
+# Khởi động JupyterLab với các tùy chọn cần thiết
+jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' --NotebookApp.password=''
